7a04312c7e3b09145085ea4c6dba101a72adf449
mashawesh <mashawesh@outlook.com>
2023-02-10 14:12:41 +0300

    Initial commit

---
+++ src/test/java/com/blu/multmodule/MultModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blu.multmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
